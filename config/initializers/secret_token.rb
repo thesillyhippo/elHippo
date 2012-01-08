07a81666e9f0fcdcd9779cb2af50885ddea6c08d
Ben Nguyen <ben@crave.com>
2012-01-08 00:46:23 -0800

    first commit for elusiveHippo project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElHippo::Application.config.secret_token = '30a8d662c6b4aba7269e020e30d0309e4e3f1be137652ef36b834f85badac6c429051979eaf01e54cd41a5dc69e714487ed6d2703920e09bfe3150f882d088fb'
