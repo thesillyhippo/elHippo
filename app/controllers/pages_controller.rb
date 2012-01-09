9b540c9c84cb75d7674ce6283097ea1c7027cd10
Ben Nguyen <ben@crave.com>
2012-01-09 02:58:38 -0800

    commit title RSpec tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,26 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def sign_in
+    @title = "Sign In"
   end
 
   def terms
+    @title = "Terms of Service"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def site_map
+    @title = "Site Map"
   end
 
   def about
+    @title = "About"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,12 +7,21 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Elusive Hippo")
+    end
   end
 
   describe "GET 'sign_in'" do
     it "should be successful" do
       get 'sign_in'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'sign_in'
+      response.should have_selector("title", :content => "Elusive Hippo | Sign In")
     end
   end
 
@@ -21,12 +30,21 @@ describe PagesController do
       get 'terms'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'terms'
+      response.should have_selector("title", :content => "Elusive Hippo | Terms of Service")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Elusive Hippo | Contact")
     end
   end
 
@@ -35,12 +53,20 @@ describe PagesController do
       get 'site_map'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'site_map'
+      response.should have_selector("title", :content => "Elusive Hippo | Site Map")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Elusive Hippo | About")
     end
   end
 
