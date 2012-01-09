793ef5a3ed94c6cb2cd7f99f57bf94663a8cbf96
Ben Nguyen <ben@crave.com>
2012-01-08 21:21:39 -0800

    first try at styling my homepage

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def sign_in
+  end
+
+  def terms
+  end
+
+  def contact
+  end
+
+  def site_map
+  end
+
+  def about
+  end
+
+end
